b149673adc7aa506ab116837726767ef60be9bc9
Māris Krivtežs <marisks@gmail.com>
2022-04-19 13:46:32 +0300

    Moved content tag update logic to tag service

--- src/Geta.Optimizely.Tags/Core/TagService.cs
+++ src/Geta.Optimizely.Tags/Core/TagService.cs
@@ -4,17 +4,25 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using EPiServer.Cms.Shell;
+using EPiServer.Core;
 using EPiServer.Data;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using Geta.Optimizely.Tags.Core.Attributes;
 
 namespace Geta.Optimizely.Tags.Core
 {
     public class TagService : ITagService
     {
         private readonly ITagRepository _tagRepository;
+        private readonly IContentTypeRepository _contentTypeRepository;
 
-        public TagService(ITagRepository tagRepository)
+        public TagService(ITagRepository tagRepository, IContentTypeRepository contentTypeRepository)
         {
             _tagRepository = tagRepository;
+            _contentTypeRepository = contentTypeRepository;
         }
 
         public Tag GetTagById(Identity id)
@@ -25,13 +33,6 @@ namespace Geta.Optimizely.Tags.Core
         public IEnumerable<Tag> GetTagsByContent(Guid contentGuid)
         {
             return _tagRepository.GetTagsByContent(contentGuid);
-        }
-
-        public Tag GetTagByName(string name)
-        {
-#pragma warning disable 618
-            return _tagRepository.GetTagByName(name);
-#pragma warning restore 618
         }
 
         public Tag GetTagByNameAndGroup(string name, string groupKey)
@@ -95,14 +96,11 @@ namespace Geta.Optimizely.Tags.Core
 
         public void Delete(string name)
         {
-            var tag = GetTagByName(name);
-
-            if (tag == null)
+            var tags = _tagRepository.GetTagsByName(name);
+            foreach (var tag in tags)
             {
-                return;
+                _tagRepository.Delete(tag);
             }
-
-            _tagRepository.Delete(tag);
         }
 
         public void Delete(Identity id)
@@ -115,6 +113,69 @@ namespace Geta.Optimizely.Tags.Core
             }
 
             _tagRepository.Delete(tag);
+        }
+
+        public void UpdateContentTags(IContent content)
+        {
+            RemoveOldContentTags(content);
+            AddContentTags(content);
+        }
+
+        private void AddContentTags(IContent content)
+        {
+            var contentType = _contentTypeRepository.Load(content.ContentTypeID);
+
+            var tagProperties = contentType.PropertyDefinitions.Where(p => p.TemplateHint == "Tags").ToArray();
+
+            if (!tagProperties.Any())
+            {
+                return;
+            }
+
+            foreach (var tagProperty in tagProperties)
+            {
+                var tagPropertyInfo = contentType.ModelType.GetProperty(tagProperty.Name);
+                var tags = GetPropertyTags(content as ContentData, tagProperty);
+
+                if (tagPropertyInfo == null)
+                {
+                    return;
+                }
+
+                var groupKeyAttribute =
+                    tagPropertyInfo.GetCustomAttribute(typeof(TagsGroupKeyAttribute)) as TagsGroupKeyAttribute;
+                var cultureSpecificAttribute
+                    = tagPropertyInfo.GetCustomAttribute(typeof(CultureSpecificAttribute)) as CultureSpecificAttribute;
+
+                var groupKey = TagsHelper.GetGroupKeyFromAttributes(groupKeyAttribute, cultureSpecificAttribute, content);
+
+                Save(content.ContentGuid, tags, groupKey);
+            }
+        }
+
+        private void RemoveOldContentTags(IContent content)
+        {
+            var contentGuid = content.ContentGuid;
+            var oldTags = GetTagsByContent(contentGuid);
+            var language = content.LanguageBranch();
+
+            foreach (var tag in oldTags)
+            {
+                if (tag.PermanentLinks == null || !tag.PermanentLinks.Contains(contentGuid) || tag.GroupKey != language)
+                {
+                    continue;
+                }
+
+                tag.PermanentLinks.Remove(contentGuid);
+
+                Save(tag);
+            }
+        }
+
+        private static IEnumerable<string> GetPropertyTags(ContentData content, PropertyDefinition propertyDefinition)
+        {
+            var tagNames = content[propertyDefinition.Name] as string;
+            return tagNames?.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries) ?? Enumerable.Empty<string>();
         }
     }
 }
